92876acbf8eb0b32aef523fc1ae4547f74a8e3f6
Duke <duke@riseup.net>
2014-08-15 02:38:49 -0300

    updated deputies about

--- parser/deputies_about.go
+++ parser/deputies_about.go
@@ -1,6 +1,15 @@
 package parser
 
-import "github.com/camarabook/camarabook-api/models"
+import (
+	"log"
+	"strings"
+	"time"
+
+	"github.com/PuerkitoBio/goquery"
+	"github.com/camarabook/camarabook-api/models"
+	"github.com/camarabook/go-popolo"
+	"gopkg.in/mgo.v2/bson"
+)
 
 type SaveDeputiesAbout struct {
 }
@@ -8,19 +17,27 @@ type SaveDeputiesAbout struct {
 func (p SaveDeputiesAbout) Run(DB models.Database) {
 	var ds []models.Parliamentarian
 
-	DB.Find(&ds)
+	DB.FindAll(&ds)
 
 	for _, d := range ds {
-		dURL := "http://www2.camara.leg.br/deputados/pesquisa/layouts_deputados_biografia?pk=" + strconv.Itoa(int(d.RegisterId))
+		id := getIdDeputado(d.Identifiers)
+		bioURL := "http://www2.camara.leg.br/deputados/pesquisa/layouts_deputados_biografia?pk=" + id
+		source := popolo.Source{
+			Url:  toPtr(bioURL),
+			Note: toPtr("Pesquisa Câmara"),
+		}
 
 		var doc *goquery.Document
 		var e error
 
-		if doc, e = goquery.NewDocument(dURL); e != nil {
+		if doc, e = goquery.NewDocument(bioURL); e != nil {
 			log.Fatal(e)
 		}
 
-		doc.Find("#bioDeputado .bioOutros").Each(func(i int, s *goquery.Selection) {
+		bio := doc.Find("#bioDeputado .bioOutros")
+
+		biographyItems := make([]string, 0)
+		bio.Each(func(i int, s *goquery.Selection) {
 			title := s.Find(".bioOutrosTitulo").Text()
 			if title != "" {
 				title = strings.TrimSpace(title)
@@ -28,18 +45,36 @@ func (p SaveDeputiesAbout) Run(DB models.Database) {
 
 				body := s.Find(".bioOutrosTexto").Text()
 
-				hasher := md5.New()
-				hasher.Write([]byte(title))
-				sectionKey := hex.EncodeToString(hasher.Sum(nil))
-
-				DB.Where(models.ParliamentarianAbout{
-					SectionKey:        string(sectionKey),
-					ParliamentarianId: d.RegisterId,
-				}).Assign(models.ParliamentarianAbout{
-					Body:  body,
-					Title: title,
-				}).FirstOrCreate(&models.ParliamentarianAbout{})
+				biographyItems = append(biographyItems, title)
+				biographyItems = append(biographyItems, body)
+				biographyItems = append(biographyItems, "")
 			}
 		})
+
+		_, err := DB.Upsert(bson.M{"id": d.Id}, bson.M{
+			"$setOnInsert": bson.M{
+				"createdat": time.Now(),
+			},
+			"$currentDate": bson.M{
+				"updatedat": true,
+			},
+			"$set": bson.M{
+				"summary":   bio.Eq(0).Find(".bioOutrosTexto").Text(),
+				"biography": strings.Join(biographyItems, "\n"),
+			},
+			"$addToSet": bson.M{
+				"sources": source,
+			},
+		}, models.Parliamentarian{})
+		checkError(err)
 	}
+}
+
+func getIdDeputado(ids []popolo.Identifier) string {
+	for _, id := range ids {
+		if *id.Scheme == "ideCadastro" {
+			return *id.Identifier
+		}
+	}
+	panic("not found id")
 }
