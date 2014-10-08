146197853db65b5f84ba981a3fcede82a9def5d3
Duke <duke@riseup.net>
2014-10-08 20:16:54 -0300

    added more log

--- go_bot/parliamentarian_bot/runner.go
+++ go_bot/parliamentarian_bot/runner.go
@@ -3,16 +3,28 @@ package main
 import (
 	"github.com/databr/api/database"
 	"github.com/databr/bots/go_bot/parliamentarian_bot/bot"
+	"github.com/databr/bots/go_bot/parser"
 )
 
 func main() {
 	mongo := database.NewMongoDB()
 
+	parser.Log.Debug("Running bot.SaveDeputiesAbout{}")
 	bot.SaveDeputiesAbout{}.Run(mongo)
+
+	parser.Log.Debug("Running bot.SaveDeputiesFromSearch{}")
 	bot.SaveDeputiesFromSearch{}.Run(mongo)
+
+	parser.Log.Debug("Running bot.SaveDeputiesFromTransparenciaBrasil{}")
 	bot.SaveDeputiesFromTransparenciaBrasil{}.Run(mongo)
+
+	parser.Log.Debug("Running bot.SaveDeputiesFromXML{}")
 	bot.SaveDeputiesFromXML{}.Run(mongo)
+
 	//	bot.SaveDeputiesQuotas{}.Run(mongo)
+	parser.Log.Debug("Running bot.SavePartiesFromTSE{}")
 	bot.SavePartiesFromTSE{}.Run(mongo)
+
+	parser.Log.Debug("Running bot.SaveSenatorsFromIndex{}")
 	bot.SaveSenatorsFromIndex{}.Run(mongo)
 }
