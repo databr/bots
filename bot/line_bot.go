95027704e96585e5d4961bea79292af5b335ea13
Duke <duke@riseup.net>
2014-10-09 00:41:17 -0300

    fixed parser file

--- bot/line_bot.go
+++ bot/line_bot.go
@@ -1,64 +1,8 @@
 package bot
 
-import (
-	"strings"
-	"time"
-
-	"github.com/PuerkitoBio/goquery"
-	"github.com/databr/api/database"
-	"github.com/databr/api/models"
-	"github.com/databr/bots/go_bot/parser"
-	"gopkg.in/mgo.v2/bson"
-)
+import "github.com/databr/api/database"
 
 type LineBot struct{}
 
 func (_ LineBot) Run(db database.MongoDB) {
-	// doc, err := goquery.NewDocument("http://www.metro.sp.gov.br/Sistemas/direto-do-metro/diretoDoMetro.aspx")
-	doc, err := goquery.NewDocument("http://www.metro.sp.gov.br/sistemas/direto-do-metro-via4/index.aspx")
-
-	parser.CheckError(err)
-
-	doc.Find("#diretoMetro ul li").Each(func(_ int, s *goquery.Selection) {
-		lineName := s.Find(".linha").Text()
-		status := strings.TrimSpace(s.Find(".status").Text())
-
-		uri := models.MakeUri(lineName)
-
-		//Status.create! name: status_name, updated_at: Time.now, line_id: line.id
-		q := bson.M{"id": uri}
-
-		_, err := db.Upsert(q, bson.M{
-			"$setOnInsert": bson.M{
-				"createdat": time.Now(),
-			},
-			"$currentDate": bson.M{
-				"updatedat": true,
-			},
-			"$set": bson.M{
-				"name": lineName,
-			},
-		}, models.Line{})
-		parser.CheckError(err)
-
-		statusQ := bson.M{"line_id": uri}
-		_, err = db.Upsert(statusQ, bson.M{
-			"$setOnInsert": bson.M{
-				"createdat": time.Now(),
-			},
-			"$currentDate": bson.M{
-				"updatedat": true,
-			},
-			"$set": bson.M{
-				"status":  status,
-				"line_id": uri,
-			},
-		}, models.Status{})
-		parser.CheckError(err)
-
-		parser.Log.Debug(lineName + " - " + status)
-		parser.Log.Info("-- Created Status to " + lineName)
-		parser.Log.Info("Status: " + status)
-		parser.Log.Info("------")
-	})
 }
